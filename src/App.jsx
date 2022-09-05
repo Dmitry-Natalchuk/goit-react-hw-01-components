40021b75362e4cef7afb77f8ee4ab2d53eb0ce53
Dmitry Natalchuk <next1996@ukr.net>
2022-09-05 19:26:04 +0300

    start react

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Profile from 'components/Profile/Profile'
+import user from 'components/Profile/user.json'
+
+
+const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}>
+      <Profile
+      username = {user.username}
+      tag = {user.tag}
+      location = {user.location}
+      avatar = {user.avatar}
+      stats = {user.stats}
+      />
+
+    </div>
+  );
+};
+
+export default App
